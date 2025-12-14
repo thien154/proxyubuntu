44886e16bad57e3f0357ab910e3eb04f605226c0
thien154 <thien154@gmail.com>
2025-12-15 00:22:05 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -61,8 +61,8 @@ install_3proxy() {
   mkdir -p "$BUILD_DIR"
   cd "$BUILD_DIR"
 
-  # 🔹 Tải tar.gz chính xác từ GitHub release
-  URL="https://github.com/z3APA3A/3proxy/archive/0.8.6.tar.gz"
+  # 🔹 Tải tar.gz từ GitHub chính xác
+  URL="https://github.com/3proxy/3proxy/archive/0.8.6.tar.gz"
   TAR_FILE="3proxy-0.8.6.tar.gz"
 
   echo "[+] Downloading 3proxy source..."
